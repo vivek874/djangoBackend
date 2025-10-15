ddb2a008c9baf7bffa74b1cfbcca824d791607a5
Vivek <vivekdahal230@gmail.com>
2025-10-15 15:59:58 +0545

    cors

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "myapp.CustomUser" 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -59,7 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 CORS_ALLOWED_ORIGINS = [
      'https://djangobackend-8bb8.onrender.com',
