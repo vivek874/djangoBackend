72e767b63c47ebe3d73165020485487273490634
Vivek <vivekdahal230@gmail.com>
2025-10-15 18:51:47 +0545

    postgres

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,10 +120,7 @@ WSGI_APPLICATION = "demo.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
